09f824052d0baf9b286295e3dca70183c2ad60e0
Dinesh Nimmagadda <dnimmagadda@detectronic.org>
2024-05-28 12:35:58 +0100

    added threading safety

--- src/logger/logger.py
+++ src/logger/logger.py
@@ -1,8 +1,19 @@
 import logging
 from typing import Callable, Optional
+from threading import Lock
 
 
 class Logger:
+    _instances = {}
+    _lock = Lock()
+
+    def __new__(cls, function_name: str, *args, **kwargs):
+        with cls._lock:
+            if function_name not in cls._instances:
+                instance = super().__new__(cls)
+                cls._instances[function_name] = instance
+            return cls._instances[function_name]
+
     def __init__(
         self,
         function_name: str,
@@ -17,6 +28,10 @@ class Logger:
             log_level (int): The log level (e.g., logging.DEBUG, logging.INFO).
             emit_func (Optional[Callable[[str], None]]): If provided, log messages will be emitted to this function.
         """
+        if hasattr(self, "initialized") and self.initialized:
+            return
+        self.initialized = True
+
         self.logger = logging.getLogger(function_name)
         self.logger.setLevel(log_level)
 
@@ -43,19 +58,21 @@ class Logger:
                 self.logger.addHandler(gui_handler)
 
     def info(self, message: str):
+        """Logs an info message."""
         self.logger.info(message)
 
     def warning(self, message: str):
+        """Logs a warning message."""
         self.logger.warning(message)
 
     def error(self, message: str):
+        """Logs an error message."""
         self.logger.error(message)
 
     def exception(self, message: str):
+        """Logs an exception message with traceback."""
         self.logger.exception(message)
 
     def debug(self, message: str):
+        """Logs a debug message."""
         self.logger.debug(message)
-
-
-logger = Logger(__name__)
